c8c5cd604b5c61422a98158e37d20109372e3f5c
StanislavPB <perminovsb@gmail.com>
2023-11-20 09:00:21 +0000

    Initial commit

---
+++ src/test/java/org/group28projectjpa/Group28ProjectJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.group28projectjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group28ProjectJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
