e222a95cf0317e9c4a3bbf4a227a88eb6141af70
StanislavPB <perminovsb@gmail.com>
2023-11-24 08:43:43 +0000

    add change controllers

--- src/main/java/org/group28projectjpa/repository/ManagerRepository.java
+++ src/main/java/org/group28projectjpa/repository/ManagerRepository.java
@@ -1,9 +1,12 @@
 package org.group28projectjpa.repository;
 
 import org.group28projectjpa.domain.entity.Manager;
+import org.group28projectjpa.domain.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
