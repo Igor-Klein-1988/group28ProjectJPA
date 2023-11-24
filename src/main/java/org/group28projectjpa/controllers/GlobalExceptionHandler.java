a7c1d508a7a52b6a7f6651359f502d7a51828e2a
StanislavPB <perminovsb@gmail.com>
2023-11-24 10:17:04 +0000

    add change controllers

--- src/main/java/org/group28projectjpa/controllers/GlobalExceptionHandler.java
+++ src/main/java/org/group28projectjpa/controllers/GlobalExceptionHandler.java
@@ -6,12 +6,14 @@ import org.group28projectjpa.core.validation.IsAlreadyExistException;
 import org.group28projectjpa.core.validation.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -39,5 +41,15 @@ public class GlobalExceptionHandler {
                 .map(ConstraintViolation::getMessage)
                 .collect(Collectors.joining(", "));
         return new ResponseEntity<>(listOfErrors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        List<String> errors = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(ObjectError::getDefaultMessage)
+                .toList();
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
